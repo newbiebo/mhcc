ffafc59890eb88d15a57e299cd76ff949b1de496
admin <1186644190wxb@gamil.com>
2024-08-27 17:57:38 +0800

    [build001] add grpc dependency

---
+++ mhcc-web/src/main/java/com/mhcc/WebApplication.java
@@ -0,0 +1,21 @@
+package com.mhcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+        System.out.println(
+                "\n" +
+                "            _                             _           _             _   _ \n" +
+                "           | |                           | |         | |           | | | |\n" +
+                "  _ __ ___ | |__   ___ ___  __      _____| |__    ___| |_ __ _ _ __| |_| |\n" +
+                " | '_ ` _ \\| '_ \\ / __/ __| \\ \\ /\\ / / _ \\ '_ \\  / __| __/ _` | '__| __| |\n" +
+                " | | | | | | | | | (_| (__   \\ V  V /  __/ |_) | \\__ \\ || (_| | |  | |_|_|\n" +
+                " |_| |_| |_|_| |_|\\___\\___|   \\_/\\_/ \\___|_.__/  |___/\\__\\__,_|_|   \\__(_)\n" +
+                "                                                                          \n" +
+                "                                                                          \n");
+    }
+}
